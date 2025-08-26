1e0a0ceff64470fcd300925f125fe59710a7bb1e
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-08-26 17:21:27 -0300

    DELETE: some components

--- src/components/AddButton/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-
-export const Container = styled.TouchableOpacity.attrs({
-  activeOpacity: 0.75,
-})`
-  position: absolute;
-  bottom: 32px;
-  right: 16px;
-`;
