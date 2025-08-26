17c02102759132f223d168b51f68ca1f77c56d79
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-08-26 14:07:43 -0300

    ADD: user store

---
+++ src/stores/UserStore.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+
+import { useUserDatabase } from "@src/database/useUserDatabase";
+import { IUser } from "@src/common/entities/User";
+
+interface UserState {
+  user: IUser | null;
+  isLoading: boolean;
+  setUser: (user: IUser | null) => void;
+  setLoading: (loading: boolean) => void;
+}
+
+export const useUserStore = create<UserState>((set) => ({
+  user: null,
+  isLoading: false,
+  setUser: (user) => set({ user }),
+  setLoading: (loading) => set({ isLoading: loading }),
+}));
