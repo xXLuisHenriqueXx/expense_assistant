f3d87e7adc0b38e86dce830514dba77700943e02
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-11-11 14:15:53 -0300

    ADD: home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import ContainerMain from "@src/components/ContainerMain";
+import Header from "./_components/Header";
+import AddAccountModal from "./_components/AddAccountModal";
+import UserAccounts from "./_components/UserAccounts";
+
+import { useUserStore } from "@src/stores/UserStore";
+
+const Home = () => {
+  const { user } = useUserStore();
+
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  if (!user) return;
+
+  return (
+    <ContainerMain rowGap={32}>
+      <Header name={user.name} />
+
+      <UserAccounts
+        id={user.id}
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
+
+      <AddAccountModal
+        id={user.id}
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
+    </ContainerMain>
+  );
+};
+
+export default Home;
