0d9d97f39909f0806bdbe20d3a92440c2aa78e9e
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-11-14 20:15:44 -0300

    ADD: width and status bar height constants

---
+++ src/constants/Values.ts
@@ -0,0 +1,6 @@
+import { Dimensions } from "react-native";
+import Constants from "expo-constants";
+
+export const WIDTH = Dimensions.get("window").width;
+
+export const STATUS_BAR_HEIGHT = Constants.statusBarHeight;
